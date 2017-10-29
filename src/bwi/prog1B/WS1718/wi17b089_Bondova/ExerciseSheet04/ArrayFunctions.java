b944545174139c36ed29b6e1a793225585f35478
Anne <Anne@AB>
2017-10-30 00:30:08 +0100

    ExerciseSheet04: ArrayFunctions

--- src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet04/ArrayFunctions.java
+++ src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet04/ArrayFunctions.java
@@ -1,5 +1,9 @@
 package bwi.prog1B.WS1718.wi17b089_Bondova.ExerciseSheet04;
 import bwi.prog.utils.TextIO;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 public class ArrayFunctions {
 
 	public static void main(String[] args) {
@@ -41,7 +45,30 @@ public class ArrayFunctions {
 		return -1;
 	}
 	public static int find(int[] arr, int key, int startIdx) {
-		
-	}
+		if (startIdx < 0) {
+			startIdx = 0;
+		}
 
+		for (int i = startIdx; i < arr.length; i++) {
+			if (arr[i] == key) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+	public static String[] filter(String[] arr, String regex) {
+		List<String> allMatches = new ArrayList<String> ();
+		allMatches.clear();
+		
+		for (int i = 0; i < arr.length; i++) {
+			Matcher m = Pattern.compile("regex")
+				     .matcher(arr[i]);
+				 while (m.find()) {
+				   allMatches.add(m.group());
+				 }	
+		}
+		return allMatches.toArray(new String[allMatches.size()]);
+	}
 }
+	
