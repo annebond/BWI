7d156b55ead81f2024b2fad3889d7b2b71322ab6
anb <anb@NB000667.sbb01.spoc.global>
2017-11-20 11:31:17 +0100

    added examples and task defnition of U6

--- src/bwi/prog1B/WS1718/wi17b089_Bondova/ALG/ALG6.java
+++ src/bwi/prog1B/WS1718/wi17b089_Bondova/ALG/ALG6.java
@@ -1,6 +1,12 @@
 package bwi.prog1B.WS1718.wi17b089_Bondova.ALG;
 import bwi.prog.utils.TextIO;
 
+/* 
+ * U6: Entwickeln Sie ein Programm, das zwei natürliche Zahlen einliest und den größten gemeinsamen
+Teiler der zwei Zahlen auf der Konsole ausgibt. Die Berechnung sollte nach dem Euklidischen
+Algorithmus erfolgen. Dieser Algorithmus ist rekursiv.
+ */
+
 public class ALG6 {
 
 	public static void main(String[] args) {
@@ -33,6 +39,11 @@ public class ALG6 {
 		  return b;
 		else
 		  return ggT_rek(b, a%b);
+/*
+ * Examples:
+ * ggT(24,36) =* ggT(36,24 mod 36) = ggT(36,24) =* ggT(24,36 mod 24) = ggT(24,12) =*ggT(12,24 mod 12) = ggT(12,0) =* 12
+ * ggT(17,19) =* ggT(19, 17 mod 19) = ggT(19,17) =* ggT(17,19 mod 17) = ggT(17,2) =* ggT(2, 17 mod 2) = ggT(2,1) =* ggT(1, 2 mod 1) 0 ggT(1,0) =* 1 
+ */
 	}
 	public static int ggT_ite (int a, int b) {
 		int rest;
