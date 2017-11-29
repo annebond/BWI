76b4656a0048b10dce08330a2333653f9de9c952
anb <anb@NB000667.sbb01.spoc.global>
2017-11-29 15:19:46 +0100

    added space, so new tests are green again

--- src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet01/NeighborOfTheBeast.java
+++ src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet01/NeighborOfTheBeast.java
@@ -5,12 +5,12 @@ public class NeighborOfTheBeast {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int formattedNumber = 667;
-		TextIO.putf("the neighbor of the beast:%d!\n", formattedNumber); //display as decimal #
-		TextIO.putf("the neighbor of the beast:%X!\n", formattedNumber); //display as hex #
-		TextIO.putf("the neighbor of the beast:%10d!\n", formattedNumber); //display as decimal # 10char right aligned
-		TextIO.putf("the neighbor of the beast:%-10d!\n", formattedNumber); //display as decimal # 10char left aligned
-		TextIO.putf("the neighbor of the beast:%08d!\n", formattedNumber); //display as decimal # 8char with leading zeros
-		TextIO.putf("the neighbor of the beast:+%-11d!\n", formattedNumber); //display as decimal # 12char left aligned + sign
+		TextIO.putf("the neighbor of the beast: %d!\n", formattedNumber); //display as decimal #
+		TextIO.putf("the neighbor of the beast: %X!\n", formattedNumber); //display as hex #
+		TextIO.putf("the neighbor of the beast: %10d!\n", formattedNumber); //display as decimal # 10char right aligned
+		TextIO.putf("the neighbor of the beast: %-10d!\n", formattedNumber); //display as decimal # 10char left aligned
+		TextIO.putf("the neighbor of the beast: %08d!\n", formattedNumber); //display as decimal # 8char with leading zeros
+		TextIO.putf("the neighbor of the beast: +%-11d!\n", formattedNumber); //display as decimal # 12char left aligned + sign
 	}
 
 }
