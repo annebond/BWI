170a86fc68ca88e7de14cd0bfdc78a72600158f8
anb <anb@NB000667.sbb01.spoc.global>
2017-11-29 15:56:52 +0100

    added all tests

--- src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet01/Swapper.java
+++ src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet01/Swapper.java
@@ -3,13 +3,13 @@ import bwi.prog.utils.TextIO;
 public class Swapper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		int firstNumber, secondNumber, storeNumber;
 		
-		TextIO.put("enter first number (a):");
-		firstNumber = TextIO.getlnInt();
-		TextIO.put("enter second number (b):");
-		secondNumber = TextIO.getlnInt();
+		TextIO.put("enter first number (a): ");
+		firstNumber = TextIO.getInt();
+		TextIO.put("enter second number (b): ");
+		secondNumber = TextIO.getInt();
 		TextIO.putln("a=" + firstNumber + ", b=" + secondNumber);
 		
 		storeNumber = firstNumber;
@@ -17,6 +17,7 @@ public class Swapper {
 		secondNumber = storeNumber;
 		
 		TextIO.putln("swapped:\na=" + firstNumber + ", b=" + secondNumber);
+
 	}
 
 }
