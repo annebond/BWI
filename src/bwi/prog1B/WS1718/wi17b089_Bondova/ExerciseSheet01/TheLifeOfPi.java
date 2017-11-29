14e8b6a180aa9048cbc62c1158280942fd674c6a
anb <anb@NB000667.sbb01.spoc.global>
2017-11-29 16:01:06 +0100

    added spaces to TheLifeOfPi so tests are green again

--- src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet01/TheLifeOfPi.java
+++ src/bwi/prog1B/WS1718/wi17b089_Bondova/ExerciseSheet01/TheLifeOfPi.java
@@ -4,13 +4,13 @@ public class TheLifeOfPi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TextIO.putf("Pi=%f!\n", 3.14159265359);
-		TextIO.putf("Pi=%12f!\n", 3.14159265359);
-		TextIO.putf("Pi=%12.2f!\n", 3.14159265359);
-		TextIO.putf("Pi=%10.0f!\n", 3.14159265359);
-		TextIO.putf("Pi=%13.11f!\n", 3.14159265359);
-		TextIO.putf("Pi=%015f!\n", 3.14159265359);
-		TextIO.putf("Pi=%+015.2f!\n", 3.14159265359);
+		
+		TextIO.putf("Pi = %f!\n", 3.14159265359);
+		TextIO.putf("Pi = %12f!\n", 3.14159265359);
+		TextIO.putf("Pi = %12.2f!\n", 3.14159265359);
+		TextIO.putf("Pi = %10.0f!\n", 3.14159265359);
+		TextIO.putf("Pi = %13.11f!\n", 3.14159265359);
+		TextIO.putf("Pi = %015f!\n", 3.14159265359);
+		TextIO.putf("Pi = %+015.2f!\n", 3.14159265359);
 	}
-
 }
