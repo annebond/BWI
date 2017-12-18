ca3869d2b67e4a97bc7a24776eb037224a8bfba5
anb <anb@NB000667.sbb01.spoc.global>
2017-12-18 19:09:29 +0100

    added ShoeShop

---
+++ src/bwi/prog1B/WS1718/wi17b089_Bondova/TUT/Kunde.java
@@ -0,0 +1,8 @@
+package bwi.prog1B.WS1718.wi17b089_Bondova.TUT;
+
+public class Kunde {
+
+	String Name;
+	Schuhmodell[] gekaufteSchuhe;
+	
+}
---
+++ src/bwi/prog1B/WS1718/wi17b089_Bondova/TUT/ShoeShop.java
@@ -0,0 +1,5 @@
+package bwi.prog1B.WS1718.wi17b089_Bondova.TUT;
+
+public class ShoeShop {
+
+}
