d871590e448b6e1d6d60f712cdefa6dfd2f90956
Anne <Anne@AB>
2018-01-20 22:27:49 +0100

    more ES05 functions

--- src/WS1718/PROG1/ES05/LendItemFunctions.java
+++ src/WS1718/PROG1/ES05/LendItemFunctions.java
@@ -58,5 +58,57 @@ public class LendItemFunctions {
 		return compare(it1.lendDate, it2.lendDate);
 	}
 
+
+	public static int compareByLendDate(LendItem it1, LendItem it2) {
+		if (it1 == null && it2 == null)
+			return 0;
+		if (it1 == null)
+			return -1;
+		if (it2 == null)
+			return 1;
+
+		return compare(it1.lendDate, it2.lendDate);
+	}
 	
+	public static int compareByReturnDate(LendItem it1, LendItem it2) {
+		if (it1 == null && it2 == null )
+			return 0;
+		if (it1 == null)
+			return -1;
+		if (it2 == null)
+			return 1;
+
+		return compare(it1.returnDate, it2.returnDate);
+	}
+	
+	public static int compareByDescription(LendItem it1, LendItem it2) {
+		if (it1 == null && it2 == null)
+			return 0;
+		if (it1 == null)
+			return -1;
+		if (it2 == null)
+			return 1;
+
+		int res=it1.description.compareTo(it2.description);
+		if (res>0) return 1;
+		if (res<0) return -1;
+		return 0;
+	}
+	
+	public static int compare(Date d1, Date d2) {
+		if (d1 == null && d2 == null)
+			return 0;
+		if (d1 == null)
+			return -1;
+		if (d2 == null)
+			return 1;
+		
+		int res= (d1.day + d1.month * 100 + d1.year * 10000) - (d2.day + d2.month
+				* 100 + d2.year * 10000);
+		
+		if (res>0) return 1;
+		if (res<0) return -1;
+		return 0;
+
+	}
 }
