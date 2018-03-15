21c8db2a185aee0e2f69568629bf9e34bd7163c2
anb <anb@NB000637.sbb01.spoc.global>
2018-03-15 22:35:35 +0100

    class artist implemented, all tests for this class are green

--- src/SS18/PROG2/MusicLandscape/entities/Artist.java
+++ src/SS18/PROG2/MusicLandscape/entities/Artist.java
@@ -1,5 +1,19 @@
 package SS18.PROG2.MusicLandscape.entities;
 
 public class Artist {
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		if (name == "" || name == "  "){
+			this.name = null;
+		} else {
+			this.name = name;
+		}
+	}
 
 }
