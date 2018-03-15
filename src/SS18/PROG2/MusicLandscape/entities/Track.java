9603ed9d48fae3afcedc32370a166f46c3867468
anb <anb@NB000637.sbb01.spoc.global>
2018-03-15 23:31:14 +0100

    getString implemented, all tests of class Track green

--- src/SS18/PROG2/MusicLandscape/entities/Track.java
+++ src/SS18/PROG2/MusicLandscape/entities/Track.java
@@ -43,4 +43,58 @@ public class Track {
 			this.performer = performer;
 		}
 	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setWriter(Artist writer) {
+		if (writer != null) {
+			this.writer = writer;
+		}
+	}
+	
+	public void setYear(int year) {
+		if (year >= 1900 && year < 3000) {
+			this.year = year;
+		}
+	}
+	
+	public boolean writerIsKnown() {
+		return getWriter() != null && getWriter().getName() != null;
+	}
+	
+	public String getString() {
+		String formString;
+		String formMin, formSec;
+		
+		formMin = String.format("%02d", getDuration()/60);
+		formSec = String.format("%02d", getDuration()%60);
+		formString = formTitle(getTitle()) + " by " + formTitle(getWriter()) + " performed by " + formTitle(getPerformer()) + " (" + formMin + ":" + formSec + ")";
+		return formString;
+	}
+	
+	public String formTitle (String strForm) {
+		if (strForm == null || strForm.isEmpty() || strForm.equals("unknown title")) {
+			strForm = String.format("%.10s", "unknown");
+		} else {
+			strForm = String.format("%.10s", strForm);
+		}
+		if (strForm.length() > 10) {strForm = String.format("%.10s", strForm);}	
+		if (strForm.length() < 10) {strForm = String.format("%1$10s", strForm);}
+		
+		return strForm;
+	}
+	
+	public String formTitle(Artist artForm) {
+		
+		String strForm;
+		
+		if (artForm == null) {
+			strForm = String.format("%.10s", "unknown");
+		} else {
+			strForm = artForm.getName();
+		}
+		return formTitle(strForm);
+	}
 }
