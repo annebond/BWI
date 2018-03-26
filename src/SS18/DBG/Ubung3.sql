985734bd21397f14bb41b8b186002783d4770735
Bondova <anb@automic.com>
2018-03-26 21:15:26 +0200

    DBG Ubung3 Bsp. 4 & 5

--- src/SS18/DBG/Ubung3.sql
+++ src/SS18/DBG/Ubung3.sql
@@ -26,4 +26,46 @@ BEGIN
 END;
 /
 
+-- Ubung 3/4 --
+/* Schreiben Sie eine Datenbankfunktion, welche die Anzahl der verschiedenen Ortsnamen zurück gibt. 
+Sollten keine Namen verfügbar sein, dann soll -1 zurück gegeben werden.  */
 
+CREATE OR REPLACE FUNCTION AnzahlOrt
+RETURN INT
+IS
+  v_ort_anzahl INT;
+BEGIN
+  SELECT COUNT(DISTINCT bezeichnung) into v_ort_anzahl FROM ort;
+  IF v_ort_anzahl = 0 THEN
+    v_ort_anzahl := -1;
+  END IF;
+  return v_ort_anzahl;
+END;
+/
+SELECT AnzahlOrt FROM dual;
+
+-- Ubung 3/5 --
+/* Schreiben Sie eine Datenbankprozedur, welche alle Dienstränge (dienstrangID, kbz und bezeichnung) für die IDs von 100 bis 110 ausschreibt. 
+Sollte eine dienstrangID in diesem Bereich nicht vorhanden sein, dann schreiben Sie stattdessen "kein Dienstrang verfügbar für ID: xxx" aus.  */
+
+CREATE OR REPLACE PROCEDURE DienstrangAusgabe
+IS
+  v_kbz VARCHAR2(20);
+  v_bezeichnung VARCHAR2(255);
+  v_count INT;
+BEGIN
+  FOR i IN 100..110 LOOP
+      SELECT COUNT(dienstrangID) INTO v_count FROM dienstrang
+      WHERE dienstrangID = i;
+      IF v_count = 1 THEN
+            SELECT kbz, bezeichnung INTO v_kbz, v_bezeichnung FROM dienstrang
+            WHERE dienstrangID = i;
+            DBMS_OUTPUT.PUT_LINE(i || ' ' ||v_kbz || ' '  || v_bezeichnung);
+      ELSE
+        DBMS_OUTPUT.PUT_LINE('Kein Dienstrang verfügbar für ID: ' || i);
+      END IF;
+  END LOOP;
+END;
+/
+
+EXEC DienstrangAusgabe;
