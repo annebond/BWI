c7444f9f4ec67d87e799c79b9ca3536ac2710483
Bondova <anb@automic.com>
2018-04-09 21:18:59 +0200

    added comments to DBG Ubung4 T2 1 and check for no data on flugID

--- src/SS18/DBG/Ubung4_T2_1.sql
+++ src/SS18/DBG/Ubung4_T2_1.sql
@@ -1,3 +1,5 @@
+SET serveroutput ON; 
+
 CREATE OR REPLACE package test AS
 PROCEDURE gep (v_flugID NUMBER default 24);
 END test;
@@ -5,6 +7,7 @@ END test;
 
 CREATE OR REPLACE package body test AS
 PROCEDURE gep (v_flugID NUMBER default 24) IS 
+  -- erster cursor um daten für diese Zeile zu fetchen: 1. Tom Brown: <Gepäckstücke: 3 - Gesamtgewicht: 27,4kg>
   CURSOR data IS
     SELECT passagierliste.personID, vorname, nachname, count(gepaeckID) AS gAnzahl, sum(gewicht) AS sumgewicht FROM passagierliste
     LEFT JOIN person ON person.personID = passagierliste.personID
@@ -13,28 +16,35 @@ PROCEDURE gep (v_flugID NUMBER default 24) IS
     GROUP BY passagierliste.personID, vorname, nachname;
   vResult data%ROWTYPE;
   v_flugnummer VARCHAR2(6);
+  v_count NUMBER;
 
+  -- zweiter cursor der innerhalb vom ersten cursor die einzelnen gepäckstk einer person ausgeben soll
   CURSOR egep IS
   SELECT gewicht FROM gepaeck
   WHERE personID = vResult.personID AND flugID = v_flugID;
 
   BEGIN
-  SELECT flugnummer INTO v_flugnummer FROM flug
-  WHERE flugID = v_flugID;
-  DBMS_OUTPUT.PUT_LINE('Flug: ' || v_flugnummer);
-  OPEN data;
-  LOOP
-    FETCH data INTO vResult;
-    EXIT WHEN data%NOTFOUND;
-    DBMS_OUTPUT.PUT_LINE(data%ROWCOUNT || '. ' || vResult.vorname || ' ' || vResult.nachname || ': <Gepäckstücke: ' || vResult.gAnzahl || ' - Gesamtgewicht: ' || vResult.sumgewicht || 'kg>');
-  --    OPEN egep;
-        FOR vResult2 IN egep
-        LOOP
-          DBMS_OUTPUT.PUT_LINE('   Gepäck ' || egep%ROWCOUNT || ': ' || vResult2.gewicht || 'kg');
-        END LOOP;
-  --    CLOSE egep;
-  END LOOP;
-  CLOSE data;
+    -- IF um zu prüfen ob es FlugID gibt, damit kein no data error kommt
+    SELECT COUNT(flugID) INTO v_count FROM flug
+    WHERE flugID = v_flugID;
+  
+    IF v_count = 1 THEN
+      SELECT flugnummer INTO v_flugnummer FROM flug
+      WHERE flugID = v_flugID;
+      DBMS_OUTPUT.PUT_LINE('Flug: ' || v_flugnummer);
+      OPEN data;
+      LOOP
+        FETCH data INTO vResult;
+        EXIT WHEN data%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(data%ROWCOUNT || '. ' || vResult.vorname || ' ' || vResult.nachname || ': <Gepäckstücke: ' || vResult.gAnzahl || ' - Gesamtgewicht: ' || vResult.sumgewicht || 'kg>');
+            FOR vResult2 IN egep
+            LOOP
+              DBMS_OUTPUT.PUT_LINE('   Gepäck ' || egep%ROWCOUNT || ': ' || vResult2.gewicht || 'kg');
+            END LOOP;
+      END LOOP;
+      CLOSE data;
+    ELSE DBMS_OUTPUT.PUT_LINE('FlugID: ' || v_flugID || ' gibt es nicht');
+    END IF;
   END;
 END;
 /
